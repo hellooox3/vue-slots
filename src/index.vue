14659dc18e867793e53fb1d51f6ce097abf58002
hellooox3 <hellooox3@outlook.com>
2024-02-28 15:40:22 +0800

    name slot + default slot +

--- src/index.vue
+++ src/index.vue
@@ -1,20 +1,27 @@
 <template>
-    <div>
-        <AppButton2 @click="log">
-            <Icon/>
-            Hello World!
-        </AppButton2>  
-    </div>
+    <AppLayout2>
+        <template #header>Slots are awesome</template>   
+        <template v-slot:default>
+
+            <AppButton2 @click="log">
+                <Icon/>
+                Hello World!
+            </AppButton2>  
+        </template>
+        <!-- <template v-slot:footer></template> --> 
+    </AppLayout2>
 </template>
 
 <script>
 import AppButton2 from "./components2/AppButton2.vue"
 import Icon from "./components/AppIcon.vue";
+import AppLayout2 from "./components2/AppLayout2.vue";
 
 export default{
     components:{
         AppButton2,
-        Icon
+        Icon,
+        AppLayout2
     },
     methods:{
         log(){
